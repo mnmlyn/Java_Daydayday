38e0e38093ffe5825dc6d041535d7981d07faf84
mnmlyn <mnmlyn@163.com>
2019-04-17 22:56:39 +0800

    if和switch

--- 002.BasicIO/BasicIOTest.java
+++ 002.BasicIO/BasicIOTest.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 
 public class BasicIOTest {
 	public static void main(String[] args) {
+		//基本键盘输入
 		Scanner sc = new Scanner(System.in);
 		System.out.println("请输入一个整数：");
 		int a = sc.nextInt();
 		System.out.println("你输入的是：" + a);
 		
+		//if语句
 		//if (a)//Java 不能将整形隐式转换为boolean
 		if (a > 20) {
 			System.out.println("大于20");
@@ -19,6 +21,33 @@ public class BasicIOTest {
 		}
 		else {
 			System.out.println("其他");
+		}
+		
+		//switch语句
+		//可以接受的表达式类型，byte,short,char,int等基本类型
+		//		以及枚举(from JDK1.5)，String(from JDK1.7)
+		System.out.println("请输入成绩(0~100)");
+		int score = sc.nextInt();
+		switch(score/10) {
+			case 9:
+				System.out.println("优秀");
+				break;
+			case 8:
+			case 7:
+			case 6:
+				System.out.println("及格");
+				break;
+			case 5:
+			case 4:
+			case 3:
+			case 2:
+			case 1:
+			case 0:
+				System.out.println("回去等通知");
+				break;
+			default:
+				System.out.println("输入有误");
+				break;
 		}
 	}
 }
