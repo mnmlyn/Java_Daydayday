1ffd1d9ceee4d7aaa2daa28383a37d775ed97c0e
mnmlyn <mnmlyn@163.com>
2019-04-17 22:37:10 +0800

    分支语句

--- 002.BasicIO/BasicIOTest.java
+++ 002.BasicIO/BasicIOTest.java
@@ -1,4 +1,6 @@
-//基本的输入输出操作
+/* 基本的输入输出操作
+ * 分支结构，和C++没太大区别，注意判断条件不能直接是int类型
+*/
 import java.util.Scanner;
 
 public class BasicIOTest {
@@ -7,5 +9,16 @@ public class BasicIOTest {
 		System.out.println("请输入一个整数：");
 		int a = sc.nextInt();
 		System.out.println("你输入的是：" + a);
+		
+		//if (a)//Java 不能将整形隐式转换为boolean
+		if (a > 20) {
+			System.out.println("大于20");
+		}
+		else if (a %2 == 1) {
+			System.out.println("不大于20的奇数");
+		}
+		else {
+			System.out.println("其他");
+		}
 	}
 }
