88347cc335b9ac338f349b1085cf3172da5655f2
mnmlyn <mnmlyn@163.com>
2019-04-18 11:32:52 +0800

    一回首，这些题目竟然如此简单，像是捡起了童年的玩具

--- 003.ForWhile/ForWhile.java
+++ 003.ForWhile/ForWhile.java
@@ -1,9 +1,18 @@
 /*
  * 循环的使用
- * 打印100-999的所有水仙花数
+ * 1. 打印100-999的所有水仙花数
+ * 2. 打印三角形
+ * #
+ * ##
+ * ###
+ * 3. 打印99乘法表
+ * 1*1=1
+ * 1*2=2 2*2=4
+ * ...
 */
 public class ForWhile {
 	public static void main(String[] args) {
+		//水仙花数
 		for (int i=100; i < 1000; ++i) {
 			int num = i;
 			int sum = 0;
@@ -16,5 +25,21 @@ public class ForWhile {
 				System.out.print(i + " ");
 		}
 		System.out.println("");
+		
+		//三角形
+		for (int i = 1; i <= 3; ++i) {
+			for (int j = 0; j < i; ++j) {
+				System.out.print("#");
+			}
+			System.out.println("");
+		}
+		
+		//乘法表
+		for (int i = 1; i <= 9; ++i) {
+			for (int j = 1; j <= i; ++j) {
+				System.out.print(j + "x" + i + "=" + i*j + '\t');
+			}
+			System.out.println("");
+		}
 	}
 }
