d8747d61718b3ae83541dc49bcb292566dbddc74
mnmlyn <mnmlyn@163.com>
2019-05-15 21:35:46 +0800

    Spring框架,最简单Demo

---
+++ 037.SpringTest/src/com/mnmlyn/entity/Test.java
@@ -0,0 +1,14 @@
+package com.mnmlyn.entity;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Person person = context.getBean("person", Person.class);
+		System.out.println(person.toString());
+	}
+
+}
